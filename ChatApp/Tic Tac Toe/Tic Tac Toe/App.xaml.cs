d21e4bd8c58d2b72ed8c1b44705ccfa6b664666b
zarebskikacper <zarebski.kacper@gmail.com>
2022-01-29 06:22:15 +0100

    Added tic tac toe

---
+++ ChatApp/Tic Tac Toe/Tic Tac Toe/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tic_Tac_Toe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
